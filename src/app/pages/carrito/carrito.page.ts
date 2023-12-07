aabf805a7d5ca7a9cc5b8e264a0c68a683a3452f
ElAlmeyda <garalmeidario@gmail.com>
2023-12-07 17:22:56 +0000

    cantidad del carrito

--- src/app/pages/carrito/carrito.page.ts
+++ src/app/pages/carrito/carrito.page.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarritoPage implements OnInit {
 
-  constructor() { }
+  cantidad: number=1;
+  precio: number =10;
+  precio_final:number=0;
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
+  }
+
+  mas(){
+    if(this.cantidad<10){
+      this.cantidad= this.cantidad+1;
+      this.precio_final= this.precio * this.cantidad;
+    }
+  }
+
+  menos(){
+    if(this.cantidad> 1){
+      this.cantidad= this.cantidad-1;
+      this.precio_final= this.precio * this.cantidad;
+    }
+    
   }
 
 }
--- src/app/pages/tienda-dental/tienda-dental.page.ts
+++ src/app/pages/tienda-dental/tienda-dental.page.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TiendaDentalPage implements OnInit {
 
+  public items = [
+    {
+      id: 0,
+      name: "cepillo de dientes",
+      precio: 10
+    }, 
+    {
+      id: 1,
+      name: "enjuague",
+      precio: 5
+    }, 
+    {
+      id: 2,
+      name: "bracket",
+      precio: 20
+    }, 
+    {
+      id: 3,
+      name: "diente postizo",
+      precio: 10
+    },
+    {
+      id: 4,
+      name: "diente de oro",
+      precio: 1000
+    } 
+  ];
+
   constructor() { }
 
   ngOnInit() {
