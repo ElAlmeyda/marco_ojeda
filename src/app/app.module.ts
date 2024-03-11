db1861af5e99ffff053ecad58b0a03f68beaf322
ElAlmeyda <garalmeidario@gmail.com>
2024-03-11 16:18:16 +0000

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { EquipoServiceService } from './service/equipo-service.service';
 import { CommonModule, DatePipe } from '@angular/common';
 
+import { getAuth, provideAuth} from '@angular/fire/auth';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getMessaging, provideMessaging } from '@angular/fire/messaging';
+import { getPerformance, providePerformance } from '@angular/fire/performance';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { getRemoteConfig, provideRemoteConfig } from '@angular/fire/remote-config';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, CommonModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, EquipoServiceService, DatePipe],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(), AppRoutingModule, CommonModule, 
+    provideFirebaseApp(() => 
+    initializeApp({"projectId":"servicio-4f831","appId":"1:867441361379:web:4048cbd33afbfb169ee0d6","databaseURL":"https://servicio-4f831-default-rtdb.firebaseio.com","storageBucket":"servicio-4f831.appspot.com","apiKey":"AIzaSyBSx_zX2iUbKScJo__1dV9_luyxQzTpJpk","authDomain":"servicio-4f831.firebaseapp.com","messagingSenderId":"867441361379","measurementId":"G-12GCW4T1V0"})), 
+    provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), 
+    //provideAppCheck(() => { 
+      // TODO get a reCAPTCHA Enterprise here https://console.cloud.google.com/security/recaptcha?project=_ //const provider = new ReCaptchaEnterpriseProvider(/* reCAPTCHA Enterprise site key */);
+      //return initializeAppCheck(undefined, { provider, isTokenAutoRefreshEnabled: true });
+    //}), 
+  provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase()), 
+  provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), providePerformance(() => getPerformance()), provideStorage(() => getStorage()), provideRemoteConfig(() => getRemoteConfig())],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, EquipoServiceService, DatePipe, ScreenTrackingService, UserTrackingService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
